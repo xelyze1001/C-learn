f221d6c5d2317179a6dc8f783721c748ef29a658
Максим <kulieshov.maksim@lll.kpi.ua>
2026-07-18 14:09:27 +0300

    modified:   1.c modified:   2.c

--- 1.c
+++ 1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdint.h>
 int main(){
     /* Variables in C 
     int - intergers numbers, such as 123 or -123
@@ -26,12 +26,32 @@ int main(){
     character = 'A';
 
 
-    printf("number is - %d\n", number );
+    printf("number is - %d\n\n", number );
 
-    printf("fraction number is - %.3f\n", fraction);
+    printf("fraction number is - %.3f\n\n", fraction);
     
-    printf("single character is - %c\n", character);
+    printf("single character is - %c\n\n", character);
 
 
+
+    /*Fixed width integers
+    int8_t 	8 bits (1 byte) 	-128 to 127 	                                                %d
+    uint8_t 	8 bits (1 byte) 	0 to 255 	                                                %u
+    int16_t 	16 bits (2 bytes) 	-32,768 to 32,767               	                        %d
+    uint16_t 	16 bits (2 bytes) 	0 to 65,535 	                                            %u
+    int32_t 	32 bits (4 bytes) 	-2,147,483,648 to 2,147,483,647 	                        %d
+    uint32_t 	32 bits (4 bytes) 	0 to 4,294,967,295 	                                        %u
+    int64_t 	64 bits (8 bytes) 	-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 	%lld
+    uint64_t 	64 bits (8 bytes) 	0 to 18,446,744,073,709,551,615 	                        %llu*/
+
+    int8_t a = 100;        // 8-bit integer
+    int16_t b = 30000;     // 16-bit integer
+    int32_t c = 2000000;   // 32-bit integer
+    int64_t d = 9000000000; // 64-bit integer
+
+    printf("%d\n\n", a);
+    printf("%d\n\n", b);
+    printf("%d\n\n", c);
+    printf("%lld\n\n", d); // use %lld for 64-bit
     return 0;
 }
--- 2.c
+++ 2.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 
 int main(){
-    
+/*Arithmetic operations in C:
++, -, *, /, %, ++, --*/
+
+    int x = 12;
+    int y = 6;
+    int z = 9;
+    printf("x + y = %d\n\n", x + y);
+    printf("x - y = %d\n\n", x - y);
+    printf("x * y = %d\n\n", x * y);
+    printf("x / y = %d\n\n", x / y);
+    printf("x Modulus y = %d\n\n", x % y);
+
+    ++z;
+    printf("%d\n\n", z);
+
+    --z;
+    printf("%d\n\n", z);
+
+//Assignment Operators
+    x +=6;
+    y -=3;
+    printf("%d, %d", x, y);
+    // +=, -=, *=, /=, %=, &=, ^=, >>=, <<=
+
+
+/*Comparison Operators
+==, >, <, !=, >=, <= */
+    int passwordLength = 5;
+
+    printf("%d\n", passwordLength >= 8); // 0 (false), too short
+    printf("%d\n", passwordLength < 8);  // 1 (true), needs more characters
+
+
+/*Logical Operators
+&&   	AND   	x < 5 &&  x < 10     Returns 1 if both statements are true
+||  	OR 	    x < 5 || x < 4 	     Returns 1 if one of the statements is true
+! 	    NOT     !(x < 5 && x < 10) 	 Reverse the result, returns 0 if the result is 1 	*/
+    int isLoggedIn = 1;
+    int isAdmin = 0;
+
+    printf("Regular user: %d\n", isLoggedIn && !isAdmin);
+    printf("Has access: %d\n", isLoggedIn || isAdmin);
+    printf("Not logged in: %d\n", !isLoggedIn);
+
+
+
     return 0;
 }
