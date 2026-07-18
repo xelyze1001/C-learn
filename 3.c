c86203f5ccc3e9ab31620d0c264869d33fc8b07f
Максим <kulieshov.maksim@lll.kpi.ua>
2026-07-18 21:47:05 +0300

    new file:   3.c

---
+++ 3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*Conditions and If Statements
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+Equal to: a == b
+Not equal to: a != b
+
+    if (condition)
+    {
+         "code" 
+    } 
+*/
+
+int main(){
+    if (20>15)
+    {
+        printf(" is true");
+    }
+    else
+    {
+        printf(" is false");
+    }
+
+
+    int x = 20;
+    int y = 10;
+    bool z = x > y;
+    if (z)
+    {
+        printf("x is greater than y");
+    }
+    
+    
+}
